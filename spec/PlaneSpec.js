545710d2e03a771b1a1a48967215ba23396fc668
rcvink <rrvink@gmail.com>
2018-01-30 17:24:22 +0000

    Refactor test

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,25 +1,21 @@
 describe('Plane', function(){
-  describe('landing', function(){
+  beforeEach(function(){
+    airport = new Airport();
+    plane = new Plane();
+  })
 
+  describe('landing', function(){
     it ('updates location to match destination', function() {
-      airport = new Airport()
-      plane = new Plane();
       plane.land(airport);
       expect(plane.location()).toEqual(airport);
     });
-
   });
 
   describe('taking off', function() {
-
     it ('updates location to null', function() {
-      plane = new Plane();
-      airport = new Airport();
       plane.land(airport);
       plane.takeOff(airport);
       expect(plane.location()).toBeNull();
     });
-
   });
-
 });
